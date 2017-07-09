41c105fa7a53be4eeab7ebccd67ec254bf285b8b
Shao Cheng <astrohavoc@gmail.com>
2017-07-09 17:47:43 +0800

    Use the ghc wrapper to substitute original ghc in the test.

--- test/ghc-kit-boot.hs
+++ test/ghc-kit-boot.hs
@@ -1,14 +1,12 @@
 module Main where
 
 import Language.Haskell.GHC.Kit.Boot
-import qualified Language.Haskell.GHC.Kit.BuildInfo as P
 import Language.Haskell.GHC.Kit.Utils.Shell
-import System.Directory
 import System.Environment
 import System.FilePath
 
 main :: IO ()
 main = do
   unsetEnv "GHC_PACKAGE_PATH"
-  t <- getCurrentDirectory
-  runShell $ boot $ BootTask (t </> "boot-lib") P.ghc [] []
+  t <- defaultBootTask
+  runShell $ boot $ t {top = top t </> "boot-lib", ghc = "ghc-wrapper"}
