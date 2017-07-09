c27878ec28713f7d3fd22911dd80eb3b71dcaa16
Shao Cheng <astrohavoc@gmail.com>
2017-07-09 20:27:01 +0800

    derive orphan Generic instances for Core/STG, useful for tree traversal.

---
+++ src/Language/Haskell/GHC/Kit/Orphans.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Language.Haskell.GHC.Kit.Orphans where
+
+import CoreSyn
+import GHC.Generics
+import StgSyn
+
+deriving instance Generic (Bind b)
+
+deriving instance Generic (Expr b)
+
+deriving instance Generic (Tickish id)
+
+deriving instance Generic (GenStgExpr bndr occ)
+
+deriving instance Generic (GenStgRhs bndr occ)
+
+deriving instance Generic (GenStgBinding bndr occ)
+
+deriving instance Generic (GenStgTopBinding bndr occ)
