131a941700472a8486205e99a3ffa32ab2a63e2f
Shao Cheng <astrohavoc@gmail.com>
2017-08-15 14:23:43 +0800

    Port some frontend code from ghc. Can serve as the basis for structural patching on parsed ASTs.

--- src/Language/Haskell/GHC/Kit/FrontendPlugin.hs
+++ src/Language/Haskell/GHC/Kit/FrontendPlugin.hs
@@ -8,15 +8,13 @@ import Control.Monad
 import Control.Monad.IO.Class
 import GHC
 import GhcPlugins
-import Language.Haskell.GHC.Kit.Caliburn
-import Language.Haskell.GHC.Kit.Compiler
+import Hooks
 
 frontendAction :: [String] -> [(String, Maybe Phase)] -> Ghc ()
 frontendAction args targets = do
   liftIO $ putStrLn $ "args: " ++ show args
   dflags <- getSessionDynFlags
-  h <- liftIO $ initCompiler >>= toHooks
-  void $ setSessionDynFlags dflags {ghcMode = CompManager, hooks = h}
+  void $ setSessionDynFlags dflags {ghcMode = CompManager, hooks = emptyHooks}
   sequenceA [guessTarget t f | (t, f) <- targets] >>= setTargets
   sf <- load LoadAllTargets
   case sf of
--- src/Language/Haskell/GHC/Kit/Hooks/Frontend.hs
+++ src/Language/Haskell/GHC/Kit/Hooks/Frontend.hs
@@ -8,8 +8,14 @@ module Language.Haskell.GHC.Kit.Hooks.Frontend
   ) where
 
 import Control.Monad.IO.Class
+import DynFlags
+import FastString
+import Fingerprint
 import HscMain
 import HscTypes
+import Module
+import SrcLoc
+import TcBackpack
 import TcRnTypes
 
 data Frontend = Frontend
@@ -20,9 +26,42 @@ data Frontend = Frontend
 defaultFrontend :: Frontend
 defaultFrontend = Frontend {onenter = \_ -> pure (), onleave = \_ _ -> pure ()}
 
+hscSimpleIface_ ::
+     HscEnv -> TcGblEnv -> Maybe Fingerprint -> IO (ModIface, Bool, ModDetails)
+hscSimpleIface_ hsc_env tc_result mb_old_iface =
+  runHsc hsc_env $ hscSimpleIface' tc_result mb_old_iface
+
+hscTypecheck_ :: Bool -> ModSummary -> Maybe HsParsedModule -> Hsc TcGblEnv
+hscTypecheck_ keep_rn mod_summary mb_rdr_module = do
+  hsc_env <- getHscEnv
+  let hsc_src = ms_hsc_src mod_summary
+      dflags = hsc_dflags hsc_env
+      outer_mod = ms_mod mod_summary
+      mod_name = moduleName outer_mod
+      outer_mod' = mkModule (thisPackage dflags) mod_name
+      inner_mod = canonicalizeHomeModule dflags mod_name
+      src_filename = ms_hspp_file mod_summary
+      real_loc = realSrcLocSpan $ mkRealSrcLoc (mkFastString src_filename) 1 1
+  if hsc_src == HsigFile && not (isHoleModule inner_mod)
+    then ioMsgMaybe $ tcRnInstantiateSignature hsc_env outer_mod' real_loc
+    else do
+      hpm <-
+        case mb_rdr_module of
+          Just hpm -> return hpm
+          Nothing -> hscParse' mod_summary
+      tc_result0 <- tcRnModule' hsc_env mod_summary keep_rn hpm
+      if hsc_src == HsigFile
+        then do
+          (iface, _, _) <- liftIO $ hscSimpleIface_ hsc_env tc_result0 Nothing
+          ioMsgMaybe $ tcRnMergeSignatures hsc_env hpm tc_result0 iface
+        else return tc_result0
+
+hscFileFrontEnd_ :: ModSummary -> Hsc TcGblEnv
+hscFileFrontEnd_ mod_summary = hscTypecheck_ False mod_summary Nothing
+
 genericHscFrontendWith :: Frontend -> ModSummary -> Hsc FrontendResult
 genericHscFrontendWith Frontend {..} mod_summary = do
   liftIO $ onenter mod_summary
-  r <- hscFileFrontEnd mod_summary
+  r <- hscFileFrontEnd_ mod_summary
   liftIO $ onleave mod_summary r
   pure $ FrontendTypecheck r
