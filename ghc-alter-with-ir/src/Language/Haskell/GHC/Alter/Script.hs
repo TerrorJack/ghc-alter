7123ec78e8683592b0003905be0c4a6ebbd10982
Shao Cheng <astrohavoc@gmail.com>
2017-09-03 14:05:22 +0800

    Use IORef to replace TVar in Script since no complex transaction is required

--- ghc-alter-with-ir/src/Language/Haskell/GHC/Alter/Script.hs
+++ ghc-alter-with-ir/src/Language/Haskell/GHC/Alter/Script.hs
@@ -6,10 +6,10 @@ module Language.Haskell.GHC.Alter.Script
 
 import Control.Monad.IO.Class
 import Data.Functor
+import Data.IORef
 import qualified Data.Map.Strict as Map
 import DynFlags
 import GHC
-import GHC.Conc
 import Language.Haskell.GHC.Alter.BuildInfo
 import Language.Haskell.GHC.Alter.Compiler
 
@@ -20,15 +20,14 @@ compile ::
   -> [ModuleName]
   -> IO (Map.Map ModuleName IR)
 compile pkgdbs pkgs import_paths mod_names = do
-  ir_map_ref <- newTVarIO Map.empty
+  ir_map_ref <- newIORef Map.empty
   hooks <-
     toHooks $
     defaultCompiler
     { runCompiler =
         \ModSummary {..} ir ->
-          atomically $ do
-            ir_map <- readTVar ir_map_ref
-            writeTVar ir_map_ref $! Map.insert (moduleName ms_mod) ir ir_map
+          atomicModifyIORef' ir_map_ref $ \ir_map ->
+            (Map.insert (moduleName ms_mod) ir ir_map, ())
     }
   defaultErrorHandler defaultFatalMessager defaultFlushOut $
     runGhc (Just ghcLibDir) $ do
@@ -45,5 +44,5 @@ compile pkgdbs pkgs import_paths mod_names = do
       setTargets [Target (TargetModule m) True Nothing | m <- mod_names]
       sf <- load LoadAllTargets
       case sf of
-        Succeeded -> liftIO $ atomically $ readTVar ir_map_ref
+        Succeeded -> liftIO $ readIORef ir_map_ref
         Failed -> fail "GHC.load returned Failed."
